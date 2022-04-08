f2ad3ed6ba9a96bc7ad39f8f9834e62bb041a7ab
xxxjavacoder <lytvyniuk.v@donnu.edu.ua>
2022-04-08 17:51:39 +0300

    Addded vakidations, masks, page-up.

--- src/js/script.js
+++ src/js/script.js
@@ -35,4 +35,81 @@ $(document).ready(function(){
 
       toggleSlide('.catalog-item__link');
       toggleSlide('.catalog-item__back');
+
+      //Modals
+
+      $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+      });
+
+      $('[data-modal=buy]').on('click', function() {
+        $('.overlay, #order').fadeIn('slow');
+      });
+
+      $('[data-modal=buy]').each(function(i) {
+        $(this).on('click', function() {
+          $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        })
+      })
+
+      $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+      })
+
+      function validateErrors(item) {
+        $(item).validate({
+          rules: {
+            name: "required",
+            phone: "required",
+            email: {
+              required: true,
+              email: true
+            }
+          },
+          messages: {
+            name: "Введите ваше имя",
+            email: {
+              required: "Поле обязательно!",
+              email: "Введите валидний емейл name@name.com"
+            },
+            phone: "Поле обязательно!"
+          }
+        });
+      }
+
+      validateErrors('#consultation form');
+      validateErrors('#order form');
+      validateErrors('#consultation-form');
+
+      $('input[name=phone]').mask("+38(999) 999-9999");
+
+      $(window).scroll(function() {
+        if($(this).scrollTop() > 1000) {
+          $('.page-up').fadeIn();
+        }else {
+          $('.page-up').fadeOut();
+        }
+      })
+
+      $(".page-up").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+          // Prevent default anchor click behavior
+          event.preventDefault();
+    
+          // Store hash
+          var hash = this.hash;
+    
+          // Using jQuery's animate() method to add smooth page scroll
+          // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 800, function(){
+    
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+          });
+        } // End if
+      });
   });
